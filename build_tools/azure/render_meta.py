89fd1c196a85b7ce0dad34f688d2d0bbed6601ad
Aaron Smith <aaronreidsmith@gmail.com>
2020-02-14 12:36:05 -0600

    Add OUTPUT_DIR back

--- build_tools/azure/render_meta.py
+++ build_tools/azure/render_meta.py
@@ -11,6 +11,7 @@ VERSION_FILE = ROOT_DIRECTORY / 'pmdarima' / 'VERSION'
 REQUIREMENTS_FILE = ROOT_DIRECTORY / 'requirements.txt'
 
 # conda is weird about yml vs yaml, so we have to use yaml
+OUTPUT_DIR = ROOT_DIRECTORY
 OUTPUT_FILE = ROOT_DIRECTORY / 'meta.yaml'
 
 TEMPLATE_PATH = Path(__file__).parent
